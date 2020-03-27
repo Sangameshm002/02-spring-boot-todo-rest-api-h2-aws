d2d19397d449983238ff732d571c88c7a2576b05
Malakappanavar <smalakappanavar@deloitte.com>
2020-03-27 18:10:03 +0530

    First Commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+insert into todo(id, username,description,target_date,is_done)
+values(10001, 'sangappa', 'Learn JPA', sysdate(), false);
+
+insert into todo(id, username,description,target_date,is_done)
+values(10002, 'sangappa', 'Learn Data JPA', sysdate(), false);
+
+insert into todo(id, username,description,target_date,is_done)
+values(10003, 'Yogi', 'Learn Microservices', sysdate(), false);
+
+
+insert into todo(id, username,description,target_date,is_done)
+values(10004, 'Yogi', 'Learn Microservices', sysdate(), false);
